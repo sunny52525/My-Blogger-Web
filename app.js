a34f2d3e5508355d7f5097737f6029599531c32c
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-08-01 04:59:14 +0530

    Initial Commit-Firease Ui

---
+++ app.js
@@ -0,0 +1,158 @@
+
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const admin = require("firebase-admin");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+app.use(cookieParser());
+if (typeof process.env.PRIVATE_KEY !== 'string') {
+    console.log(process.env.PRIVATE_KEY);
+} else {
+    var key = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+}
+admin.initializeApp({
+    credential: admin.credential.cert({
+
+        "private_key": key,
+        "client_email": process.env.CLIENT_EMAIL,
+        "project_id": process.env.PROJECT_ID
+
+    }
+    ),
+    databaseURL: "https://my-blogger-1b264.firebaseio.com",
+    authDomain: "my-blogger-1b264.firebaseapp.com"
+
+});
+
+var db = admin.database();
+
+app.get("/posts/:postId", function (req, res) {
+    // console.log(req.params);
+    var postid = req.params.postId;
+    console.log(postid);
+    var postRef = db.ref("posts").child(postid);
+
+    postRef.on("value", snap => {
+
+        console.log(snap.val());
+
+        if (snap.val() != null) {
+            // res.send(snap.val().content);
+            res.render("home", {
+
+                title: snap.val().title,
+                content: snap.val().content,
+                author: snap.val().nameOP,
+                authorUsername: snap.val().username,
+                time: snap.val().time
+
+            });
+        }
+        else
+            res.sendFile(__dirname + "/views/404.html");
+
+        // console.log(snap.value);
+    }, function (error) {
+        console.log("The read failed " + error.code);
+    }
+    );
+});
+
+
+app.get("/signin", function (req, res) {
+
+    res.sendFile(__dirname + "/views/signin.html")
+});
+
+app.get('/sessionLogin', (req, res) => {
+
+    const idToken = req.query.idToken;
+
+    console.log("****************");
+    console.log(idToken);
+
+
+
+    console.log("*****************88");
+
+    setCookie(idToken, res);
+});
+
+
+
+
+
+app.post("/posts/", function (req, res) {
+    // console.log(req);
+    // console.log("***************8");
+    // console.log(req.body.token);
+    var token = (req.body.token);
+    admin.auth().verifyIdToken(token).then(function (decodedToken) {
+        let uid = decodedToken.uid;
+        res.redirect("/");
+        console.log(uid);
+    }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html")
+});
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + "/views/404.html");
+});
+app.listen(port, function () {
+    console.log("Server started on port on " + port);
+});
+
+function setCookie(idToken, res) {
+	// Set session expiration to 5 days.
+	// Create the session cookie. This will also verify the ID token in the process.
+	// The session cookie will have the same claims as the ID token.
+	
+	const expiresIn = 60 * 60 * 24 * 5 * 1000;
+	admin.auth().createSessionCookie(idToken, {expiresIn}).then((sessionCookie) => {
+		
+		// Set cookie policy for session cookie and set in response.
+		const options = {maxAge: expiresIn, httpOnly: true, secure: true};
+		res.cookie('__session', sessionCookie, options);
+		
+		admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+            let uid =decodedToken.uid;
+            res.redirect("/");
+            console.log("uid is "+uid);
+		});
+			
+	}, error => {
+        console.log(error);
+		res.status(401).send('UNAUTHORIZED REQUEST!');
+	});
+}
+function checkCookieMiddleware(req, res, next) {
+
+	const sessionCookie = req.cookies.__session || '';
+
+	admin.auth().verifySessionCookie(
+		sessionCookie, true).then((decodedClaims) => {
+			req.decodedClaims = decodedClaims;
+			next();
+		})
+		.catch(error => {
+			// Session cookie is unavailable or invalid. Force user to login.
+			res.redirect('/signin');
+		});
+}
---
+++ public/signin/index.js
@@ -0,0 +1,74 @@
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCMVzkxtsE3H4QR3lcVM2t_YDpH2nOalmk",
+  authDomain: "my-blogger-1b264.firebaseapp.com",
+  databaseURL: "https://my-blogger-1b264.firebaseio.com",
+  projectId: "my-blogger-1b264",
+  storageBucket: "my-blogger-1b264.appspot.com",
+  messagingSenderId: "580169673576",
+  appId: "1:580169673576:web:b6a749125f22350d2044dd",
+  measurementId: "G-5YRSFMWQKY"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+
+// FirebaseUI config.
+var uiConfig = {
+  signInSuccessUrl: 'https://www.google.com',
+  signInOptions: [
+      // google sign in option
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+
+  // Terms of service url/callback.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+      window.location.assign('<your-privacy-policy-url>');
+  },
+
+  callbacks: {
+      signInSuccess: function(user, credential, redirectUrl) {
+          // User successfully signed in.
+
+          user.getIdToken().then(function(idToken) {
+              window.location.href = '/sessionLogin?idToken=' + idToken;
+          }).catch(error => {
+              alert(error);
+          }) 
+
+      }
+  }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+
+firebase.auth().onAuthStateChanged(function (user) {
+
+  if (user) {
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    console.log(displayName);
+    console.log(user.uid);
+    window.location.href='/';
+  } else {
+
+  }
+
+});
