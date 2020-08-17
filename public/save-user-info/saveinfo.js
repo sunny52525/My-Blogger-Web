b9ce84d6eae77d52f94632a9f1e4ab1507742e0f
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-08-17 13:00:16 +0530

    deploy

--- public/save-user-info/saveinfo.js
+++ public/save-user-info/saveinfo.js
@@ -1,5 +1,5 @@
 
-  
+
 $(document).ready(function () {
 
     $('#profile').click(function () {
@@ -7,7 +7,7 @@ $(document).ready(function () {
         $('#imgInp').click();
     });
     function readURL(input) {
-      
+
         if (input.files && input.files[0]) {
             var reader = new FileReader();
 
@@ -28,46 +28,50 @@ $(document).ready(function () {
 
 
 function sendFormData() {
-    
+
     $(".error-username").removeClass("visible");
     $(".error-name").removeClass("visible");
-  $(".progress").addClass("visible");
-  document.getElementById("submitButton").disabled = true;
-  const file= $('#imgInp').prop('files')[0]; 
- var username=$('#username').val();
-     var name=$('#name').val();
-     var bio=$('#bio').val();
-  var fd = new FormData();
-  fd.append("file",file);
+    $(".progress").addClass("visible");
+    document.getElementById("submitButton").disabled = true;
+    const file = $('#imgInp').prop('files')[0];
+    var username = $('#username').val();
+    var name = $('#name').val();
+    var bio = $('#bio').val();
+    var fd = new FormData();
+    fd.append("file", file);
 
 
 
     $.ajax({
         type: "POST",
-        url: "verify?username="+username +"&name="+name+"&bio="+bio,
+        url: "verify?username=" + username + "&name=" + name + "&bio=" + bio,
         data: fd,
         contentType: false,
-    processData: false,
-        enctype:'multipart/form-data',
+        processData: false,
+        enctype: 'multipart/form-data',
         success: function (data) {
-      
+
             if (data == "username-error") {
                 $(".error-username").addClass("visible");
-                
-  document.getElementById("submitButton").disabled = false;
+                alert("Username is not available");
+                document.getElementById("submitButton").disabled = false;
             } else if (data == "username-empty") {
+
                 $(".error-username").addClass("visible");
-                
-  document.getElementById("submitButton").disabled = false;
+                alert("Username Cant be empty");
+
+                document.getElementById("submitButton").disabled = false;
             } else if (data == "name-empty") {
                 $(".error-name").addClass("visible");
-                
-  document.getElementById("submitButton").disabled = false;
-            } else if(data=="username-invalid"){
-                alert("Invalid username\n"+
-                " Username can only contain\n "
-                +"characters, Numbers"
-                + " and -(dash) , _ (hypen)"
+                alert("Name cant be empty");
+
+
+                document.getElementById("submitButton").disabled = false;
+            } else if (data == "username-invalid") {
+                alert("Invalid username\n" +
+                    " Username can only contain\n "
+                    + "characters, Numbers"
+                    + " and -(dash) , _ (hypen)"
                 );
                 $(".error-username").addClass("visible");
 
