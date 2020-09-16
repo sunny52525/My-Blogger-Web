07c3b6885c35f5c15bfd2c408d116be48df395d1
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-09-17 00:48:21 +0530

    test deploy

--- app.js
+++ app.js
@@ -549,18 +549,18 @@ app.get('*', function (req, res) {
 
 
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, app)
-    .listen(3000, function () {
-        console.log('My Bloggerlistening on port 3000! Go to https://localhost:3000/');
-    });
+// https.createServer({
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+// }, app)
+//     .listen(3000, function () {
+//         console.log('My Bloggerlistening on port 3000! Go to https://localhost:3000/');
+//     });
 
 
-// app.listen(port, function () {
-//     console.log("Server started on port" + port);
-// });
+app.listen(port, function () {
+    console.log("Server started on port" + port);
+});
 
 
 function setCookie(idToken, res) {
